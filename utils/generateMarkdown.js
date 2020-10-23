f2954434919a80cd33872a51e4238212fe9ba905
Jordan Morse <jtm6496@gmail.com>
2020-10-23 12:45:31 -0400

    writing markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,6 +24,22 @@ function generateMarkdown(data) {
   ## Table of Contents
    [Installation](##Installation)
 
-  
+   ## Installation
+   ${data.installation}
+   ## Usage
+   ${data.usage}
+   ## License
+   ${data.license}
+   ## Contributors
+   ${data.contributors}
+   ## Test
+   ${data.test}
+   ## Questions
+   If you have any questions feel free to contact me on [Github](http://github.com/${data.github}) or via [Email](${data.email}).
+   
+   
+ 
+ `;
+ }
 
 module.exports = generateMarkdown;
